8acf10f5bcfc57f2d645b851dc603a3b1370aa02
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-09-06 20:52:03 +0300

    Added socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import express from "express";
+import http from "http";
 import dotenv from "dotenv";
-
-const app = express();
+import { Server } from "socket.io";
 
 dotenv.config();
 
-console.log(`Log info: port set to: ${process.env.PORT}`);
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
