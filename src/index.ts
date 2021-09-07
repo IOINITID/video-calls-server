682a0c453abaee89f75004251929197784fd975f
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-09-07 14:07:35 +0300

    Updated server configuration

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,21 @@ import { Server } from "socket.io";
 dotenv.config();
 
 const PORT = Number(process.env.PORT) || 8080;
+const INDEX = "/index.html";
 
 const app = express();
+
+app.use((req, res) => res.sendFile(INDEX, { root: __dirname }));
+
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "https://ioinitid.github.io/video-calls/",
+    // origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
   transports: ["polling"],
-  pingInterval: 10,
+  //   pingInterval: 10,
 });
 
 io.on("connection", (socket) => {
