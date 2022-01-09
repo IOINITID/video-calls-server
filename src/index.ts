898de7cc6ef9092d8be31e1f788d4591d08bc12f
IOINITID <50173108+IOINITID@users.noreply.github.com>
2022-01-09 15:22:33 +0300

    Updated socket events

--- src/index.ts
+++ src/index.ts
@@ -35,158 +35,214 @@ app.use(isError);
 io.on('connection', (socket) => {
   // ON-CONNECT - кастомное событие
   socket.on('on-connect', async (userId: string) => {
-    const user = await User.findById(userId);
+    try {
+      const user = await User.findById(userId);
 
-    if (!user) {
-      throw ApiError.BadRequest('Пользователь не найден.');
+      if (!user) {
+        throw ApiError.BadRequest('Пользователь не найден.');
+      }
+
+      user.status = 'online';
+      user.socketId = socket.id;
+
+      await user?.save();
+
+      // socket.emit('on-connect'); // Отправка только себе
+      // socket.broadcast.emit('on-connect'); // Отправка всем клиентам кроме себя
+
+      io.emit('on-connect'); // Отправка всем клиентам в сети
+    } catch (error) {
+      console.log(error);
     }
-
-    user.status = 'online';
-    user.socketId = socket.id;
-
-    await user?.save();
-
-    // socket.emit('on-connect'); // Отправка только себе
-    // socket.broadcast.emit('on-connect'); // Отправка всем клиентам кроме себя
-
-    io.emit('on-connect'); // Отправка всем клиентам в сети
   });
 
   // ON-DISCONNECT - кастомное событие
   socket.on('on-disconnect', async (userId: string) => {
-    const user = await User.findById(userId);
+    try {
+      const user = await User.findById(userId);
 
-    if (!user) {
-      throw ApiError.BadRequest('Пользователь не найден.');
+      if (!user) {
+        throw ApiError.BadRequest('Пользователь не найден.');
+      }
+
+      user.status = 'offline';
+      user.socketId = '';
+
+      await user?.save();
+
+      // socket.emit('on-connect'); // Отправка только себе
+      // socket.broadcast.emit('on-connect'); // Отправка всем клиентам кроме себя
+
+      io.emit('on-disconnect'); // Отправка всем клиентам в сети
+    } catch (error) {
+      console.log(error);
     }
-
-    user.status = 'offline';
-    user.socketId = '';
-
-    await user?.save();
-
-    // socket.emit('on-connect'); // Отправка только себе
-    // socket.broadcast.emit('on-connect'); // Отправка всем клиентам кроме себя
-
-    io.emit('on-disconnect'); // Отправка всем клиентам в сети
   });
 
   //  DISCONNECT - событие отключение одного из пользователей
   socket.on('disconnect', async () => {
-    console.log('disconnect', socket);
+    try {
+      console.log('disconnect', socket);
 
-    const user = await User.findOne({ socketId: socket.id });
+      const user = await User.findOne({ socketId: socket.id });
 
-    if (!user) {
-      throw ApiError.BadRequest('Пользователь не найден.');
+      if (!user) {
+        throw ApiError.BadRequest('Пользователь не найден.');
+      }
+
+      user.status = 'offline';
+      user.socketId = '';
+
+      await user?.save();
+
+      // socket.emit('on-connect'); // Отправка только себе
+      // socket.broadcast.emit('on-connect'); // Отправка всем клиентам кроме себя
+
+      io.emit('on-disconnect'); // Отправка всем клиентам в сети
+    } catch (error) {
+      console.log(error);
     }
-
-    user.status = 'offline';
-    user.socketId = '';
-
-    await user?.save();
-
-    // socket.emit('on-connect'); // Отправка только себе
-    // socket.broadcast.emit('on-connect'); // Отправка всем клиентам кроме себя
-
-    io.emit('on-disconnect'); // Отправка всем клиентам в сети
   });
 
   // ON-ADD-INVITE-TO-FRIENDS
   socket.on('on-add-invite-to-friends', async (userId: string) => {
-    const user = await User.findById(userId);
+    try {
+      const user = await User.findById(userId);
 
-    if (!user) {
-      throw ApiError.BadRequest('Пользователь не найден.');
+      if (!user) {
+        throw ApiError.BadRequest('Пользователь не найден.');
+      }
+
+      socket.to(user.socketId).emit('on-add-invite-to-friends'); // Отправка пользователю, которого добавляют в друзья
+      socket.emit('on-add-invite-to-friends'); // Отправка пользователю, который добавляет в друзья
+      // TODO: Продолжить добавлять событие добавления в друзья
+    } catch (error) {
+      console.log(error);
     }
-
-    socket.to(user.socketId).emit('on-add-invite-to-friends'); // Отправка пользователю, которого добавляют в друзья
-    socket.emit('on-add-invite-to-friends'); // Отправка пользователю, который добавляет в друзья
-    // TODO: Продолжить добавлять событие добавления в друзья
   });
 
   // ON-ADD-TO-FRIENDS
   socket.on('on-add-to-friends', async (userId: string) => {
-    const user = await User.findById(userId);
+    try {
+      const user = await User.findById(userId);
 
-    if (!user) {
-      throw ApiError.BadRequest('Пользователь не найден.');
+      if (!user) {
+        throw ApiError.BadRequest('Пользователь не найден.');
+      }
+
+      socket.to(user.socketId).emit('on-add-to-friends'); // Отправка пользователю, который отправлял приглашение в друзья
+      socket.emit('on-add-to-friends'); // Отправка пользователю, который принял приглашение в друзья
+
+      // TODO: Продолжить добавлять событие добавления в друзья
+    } catch (error) {
+      console.log(error);
     }
-
-    socket.to(user.socketId).emit('on-add-to-friends'); // Отправка пользователю, который отправлял приглашение в друзья
-    socket.emit('on-add-to-friends'); // Отправка пользователю, который принял приглашение в друзья
-
-    // TODO: Продолжить добавлять событие добавления в друзья
   });
 
   // ON-REMOVE-FROM-FRIENDS - удаление из пользователей (кастомное событие)
   socket.on('on-remove-from-friends', async (userId: string) => {
-    const user = await User.findById(userId); // Пользователь которого удаляют из друзей
+    try {
+      const user = await User.findById(userId); // Пользователь которого удаляют из друзей
 
-    if (!user) {
-      throw ApiError.BadRequest('Пользователь не найден.');
+      if (!user) {
+        throw ApiError.BadRequest('Пользователь не найден.');
+      }
+
+      socket.to(user.socketId).emit('on-remove-from-friends'); // Отправка пользователю, который которого удаляют из друзей
+      socket.emit('on-remove-from-friends'); // Отправка пользователю, который удаляет из друзей
+    } catch (error) {
+      console.log(error);
     }
-
-    socket.to(user.socketId).emit('on-remove-from-friends'); // Отправка пользователю, который которого удаляют из друзей
-    socket.emit('on-remove-from-friends'); // Отправка пользователю, который удаляет из друзей
   });
 
   // ON-REMOVE-INVITE-TO-FRIENDS - отклонение приглашения в друзья (кастомное событие)
   socket.on('on-remove-invite-to-friends', async (userId: string) => {
-    // TODO: Доделать отклонение в друзья
-    // TODO: Добавить разделение на remove-invite-to-friends и remove-waiting-for-approval
-    // TODO: Отдельное API для удаления по общему признаку
-    const user = await User.findById(userId); // Пользователь который отклоняет приглашение в друзья
+    try {
+      // TODO: Доделать отклонение в друзья
+      // TODO: Добавить разделение на remove-invite-to-friends и remove-waiting-for-approval
+      // TODO: Отдельное API для удаления по общему признаку
+      const user = await User.findById(userId); // Пользователь который отклоняет приглашение в друзья
 
-    if (!user) {
-      throw ApiError.BadRequest('Пользователь не найден.');
+      if (!user) {
+        throw ApiError.BadRequest('Пользователь не найден.');
+      }
+
+      socket.to(user.socketId).emit('on-remove-invite-to-friends'); // Отправка пользователю, который которого удаляют из друзей
+      socket.emit('on-remove-invite-to-friends'); // Отправка пользователю, который удаляет из друзей
+    } catch (error) {
+      console.log(error);
     }
-
-    socket.to(user.socketId).emit('on-remove-invite-to-friends'); // Отправка пользователю, который которого удаляют из друзей
-    socket.emit('on-remove-invite-to-friends'); // Отправка пользователю, который удаляет из друзей
   });
 
   // ON-CHANNEL-JOIN - присоединение к комнате (кастомное событие)
   socket.on('on-channel-join', async (channelId: string, userId: string) => {
-    const user = await User.findById(userId); // Пользователь который вошел в канал
+    try {
+      const user = await User.findById(userId); // Пользователь который вошел в канал
 
-    if (!user) {
-      throw ApiError.BadRequest('Пользователь не найден.');
+      if (!user) {
+        throw ApiError.BadRequest('Пользователь не найден.');
+      }
+
+      socket.join(channelId); // Присоединение пользователя к каналу
+
+      // TODO: Добавить имя пользователя в сообщение
+      socket.to(channelId).emit('on-channel-join', `${user.name} has joined.`); // Отправка пользователям, которые находятся в одном канале
+      socket.emit('on-channel-join', `${user.name} has joined.`); // Отправка пользователю, который вошел в канал
+    } catch (error) {
+      console.log(error);
     }
-
-    socket.join(channelId); // Присоединение пользователя к каналу
-
-    // TODO: Добавить имя пользователя в сообщение
-    socket.to(channelId).emit('on-channel-join', `${user.name} has joined.`); // Отправка пользователям, которые находятся в одном канале
-    socket.emit('on-channel-join', `${user.name} has joined.`); // Отправка пользователю, который вошел в канал
   });
 
   // ON-CALL - звонок пользователю
   socket.on('on-call', async (userId, userIdToCall, signalData) => {
-    const user = await User.findById(userId);
+    try {
+      const user = await User.findById(userId);
 
-    if (!user) {
-      throw ApiError.BadRequest('Пользователь не найден.');
+      if (!user) {
+        throw ApiError.BadRequest('Пользователь не найден.');
+      }
+
+      const userToCall = await User.findById(userIdToCall);
+
+      if (!userToCall) {
+        throw ApiError.BadRequest('Пользователь которому вы звоните не найден.');
+      }
+
+      socket.to(userToCall.socketId).emit('on-call', signalData, user.id);
+    } catch (error) {
+      console.log(error);
     }
-
-    const userToCall = await User.findById(userIdToCall);
-
-    if (!userToCall) {
-      throw ApiError.BadRequest('Пользователь которому вы звоните не найден.');
-    }
-
-    socket.to(userToCall.socketId).emit('on-call', signalData, user.id);
   });
 
   // ON-CALL-ANSWER - принятие звонока от пользователя
   socket.on('on-call-answer', async (signalData, userId) => {
-    const user = await User.findById(userId);
+    try {
+      const user = await User.findById(userId);
 
-    if (!user) {
-      throw ApiError.BadRequest('Пользователь не найден.');
+      if (!user) {
+        throw ApiError.BadRequest('Пользователь не найден.');
+      }
+
+      socket.to(user.socketId).emit('on-call-answer', signalData);
+    } catch (error) {
+      console.log(error);
     }
+  });
 
-    socket.to(user.socketId).emit('on-call-answer', signalData);
+  // ON-CALL-END - окончание вызова от пользователя
+  socket.on('on-call-end', async (userId) => {
+    try {
+      const user = await User.findById(userId);
+
+      if (!user) {
+        throw ApiError.BadRequest('Пользователь не найден.');
+      }
+
+      socket.to(user.socketId).emit('on-call-end'); // Отправка события окончания вызова пользователю который принмал вызов
+      socket.emit('on-call-end'); // Отправка события окончания вызова пользователю который звонил
+    } catch (error) {
+      console.log(error);
+    }
   });
 });
 
