60aad3b6077c31012399ecc3d5becd9587a29c52
IOINITID <50173108+IOINITID@users.noreply.github.com>
2022-05-15 19:40:49 +0300

    Updated core router

--- src/core/router/index.ts
+++ src/core/router/index.ts
@@ -1,35 +1,35 @@
-import { Router } from 'express';
-import { userController } from '../controllers';
-import { isAuthorizatedMiddleware } from '../middlewares';
+// import { Router } from 'express';
+// import { userController } from '../controllers';
+// import { isAuthorizatedMiddleware } from '../middlewares';
 
-const router = Router();
+// const router = Router();
 
-router.get('/activate/:link', userController.activate);
+// router.get('/activate/:link', userController.activate);
 
-router.get('/friends', isAuthorizatedMiddleware, userController.getFriends);
+// router.get('/friends', isAuthorizatedMiddleware, userController.getFriends);
 
-router.get('/invites', isAuthorizatedMiddleware, userController.getInvites);
+// router.get('/invites', isAuthorizatedMiddleware, userController.getInvites);
 
-router.get('/approvals', isAuthorizatedMiddleware, userController.getApprovals);
+// router.get('/approvals', isAuthorizatedMiddleware, userController.getApprovals);
 
-router.post('/add-invite-to-friends', isAuthorizatedMiddleware, userController.addInviteToFriends);
+// router.post('/add-invite-to-friends', isAuthorizatedMiddleware, userController.addInviteToFriends);
 
-router.post('/remove-invite-to-friends', isAuthorizatedMiddleware, userController.removeInviteToFriends);
+// router.post('/remove-invite-to-friends', isAuthorizatedMiddleware, userController.removeInviteToFriends);
 
-router.post('/add-to-friends', isAuthorizatedMiddleware, userController.addToFriends);
+// router.post('/add-to-friends', isAuthorizatedMiddleware, userController.addToFriends);
 
-router.post('/remove-from-friends', isAuthorizatedMiddleware, userController.removeFromFriends);
+// router.post('/remove-from-friends', isAuthorizatedMiddleware, userController.removeFromFriends);
 
-router.post('/add-channel', isAuthorizatedMiddleware, userController.addChannel);
+// router.post('/add-channel', isAuthorizatedMiddleware, userController.addChannel);
 
-router.get('/get-channels', isAuthorizatedMiddleware, userController.getChannels);
+// router.get('/get-channels', isAuthorizatedMiddleware, userController.getChannels);
 
-router.post('/add-message-to-channel', isAuthorizatedMiddleware, userController.addMessageToChannel);
+// router.post('/add-message-to-channel', isAuthorizatedMiddleware, userController.addMessageToChannel);
 
-router.post('/get-channel-messages', isAuthorizatedMiddleware, userController.getChannelMessages);
+// router.post('/get-channel-messages', isAuthorizatedMiddleware, userController.getChannelMessages);
 
-router.post('/add-personal-messages-channel', isAuthorizatedMiddleware, userController.addPersonalMessagesChannel);
+// router.post('/add-personal-messages-channel', isAuthorizatedMiddleware, userController.addPersonalMessagesChannel);
 
-router.post('/get-personal-messages-channels', isAuthorizatedMiddleware, userController.getPersonalMessagesChannels);
+// router.post('/get-personal-messages-channels', isAuthorizatedMiddleware, userController.getPersonalMessagesChannels);
 
-export const defaultRouter = router;
+// export const defaultRouter = router;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { json, urlencoded } from 'express';
+import express, { json } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
