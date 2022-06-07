78e6c4faea832505e6064f0886346e1e976898ca
IOINITID <50173108+IOINITID@users.noreply.github.com>
2022-06-07 20:03:07 +0300

    Updated sent invitation socket

--- src/core/sockets/sent-invitation-socket.ts
+++ src/core/sockets/sent-invitation-socket.ts
@@ -7,27 +7,36 @@ import { getInvitationsService } from 'modules/invitations/services';
 const sentInvitationSocket = (io: Server, socket: Socket) => {
   socket.on('client:sent_invitation', async (userId: string, friendId: string) => {
     try {
+      // NOTE: Пользователь который отправил приглашение в друзья
       const user = await pool.query('SELECT * FROM users WHERE id = $1', [userId]);
 
+      // NOTE: Проверка на то, что пользователь который отправил приглашение в друзья зарегистрирован
       if (!user.rows[0]) {
         throw ApiError.BadRequest('Пользователь который отправил приглашение в друзья не найден.');
       }
 
+      // NOTE: Пользователь которому отправили приглашение в друзья
       const friend = await pool.query('SELECT * FROM users WHERE id = $1', [friendId]);
 
+      // NOTE: Проверка на то, что пользователь которому отправили приглашение в друзья зарегистрирован
       if (!friend.rows[0]) {
         throw ApiError.BadRequest('Пользователь которому отправили приглашение в друзья не найден.');
       }
 
       // TODO: Исправить это на клиенте, отправлять событие когда получен успешный ответ от сервера
+
       // NOTE: Ожидание когда данные будут обновлены
       await getInvitationsService({ user_id: userId });
+      // NOTE: Ожидание когда данные будут обновлены
       await getInvitationsService({ user_id: friendId });
 
-      socket.emit('server:sent_invitation'); // Отправка пользователю который добавляет в друзья
-      socket.to(friend.rows[0].socket_id).emit('server:sent_invitation'); // Отправка пользователю которого добавляют в друзья
+      // NOTE: Отправка пользователю который отправил приглашение в друзья
+      socket.emit('server:sent_invitation');
+
+      // NOTE: Отправка пользователю которому отправили приглашение в друзья
+      socket.to(friend.rows[0].socket_id).emit('server:sent_invitation');
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   });
 };
